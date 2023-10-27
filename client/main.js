e413b45e341169405398199e175d8d70f1d2c968
Pablo Silva <pmarcelosilvag@gmail.com>
2023-10-27 02:52:03 -0400

    add: config client socket.io

---
+++ client/main.js
@@ -0,0 +1,8 @@
+
+/**
+ * Conectamos el cliente con el server
+ */
+//http://192.168.0.3:3077
+//http:localhost:3077
+
+var socket=io('http://localhost:3077',{'forceNew':true})
---
+++ server/index.js
@@ -0,0 +1,33 @@
+
+  /**
+   * Configuramos el servidor http con express
+   */ 
+ const express= require('express')
+ const app=express()
+ const server= require('http').Server(app)
+ const io=require('socket.io')(server)
+ const PORT=3077
+ const path=require('path')
+ 
+ /**
+  * Configuracion del directorio estatico para archivos estaticos, en este ejemplo index.html
+  */
+ app.use(express.static(path.join(__dirname, 'client')));
+
+ /**
+  * Evento que se ejecuta cuando se conecta un nuevo usuario al socket
+  */
+ io.on('connection',(socket)=>{
+    console.log(`Usuario con IP:${socket.handshake.address} se conecto`);
+    
+ })
+
+
+ app.get('/server',(req,res)=>{
+    res.status(200).send('SERVER: OK')
+ })
+ 
+ app.listen(PORT,()=>{
+    console.log(`Server es running at port: ${PORT}`);
+ })
+ 
